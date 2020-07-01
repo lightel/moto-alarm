80f4bc132c3613e237d0b57561f65ce08c6112bc
Alexey Andrushkevich <andrushkevich@gmail.com>
2020-07-01 12:12:37 +0300

    use YOLOv3 to recognize objects

--- moto_alarm.py
+++ moto_alarm.py
@@ -1,27 +1,15 @@
 import os
 import cv2
 import time, datetime
-import mrcnn
-import mrcnn.config
-import mrcnn.utils
 import queue
 import threading
 import numpy as np
 import winsound
-from mrcnn.model import MaskRCNN
-from mrcnn import visualize
+import sys
+import urllib.request
 from pathlib import Path
 
 VIDEO_SOURCE = "./assets/sample1.mp4" # put here URL to the video stream
-
-###########################################################################################################################
-# Configuration that will be used by the Mask-RCNN library
-class MaskRCNNConfig(mrcnn.config.Config):
-    NAME = "coco_pretrained_model_config"
-    IMAGES_PER_GPU = 1
-    GPU_COUNT = 1
-    NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
-    DETECTION_MIN_CONFIDENCE = 0.6
 
 ###########################################################################################################################
 # Bufferless VideoCapture
@@ -52,27 +40,31 @@ class VideoCapture:
             return self.q.get(block=True, timeout=5)
         except queue.Empty:
             return None
-    
 ###########################################################################################################################
-def get_other_objects_boxes(boxes, class_ids):
-    object_boxes = []
+#
+def getIoU(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
 
-    for i, box in enumerate(boxes):
-        # If the detected object is motorcycle, skip it
-        if class_ids[i]!=4:
-            object_boxes.append(box)
+    # compute the area of intersection rectangle
+    interArea = (xB - xA) * (yB - yA)
 
-    return np.array(object_boxes)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+    boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
 
-def get_motorcycle_boxes(boxes, class_ids, scores):
-    motorcycle_boxes = []
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
 
-    for i, box in enumerate(boxes):
-        # If the detected object isn't a motorcycle, skip it
-        if class_ids[i]==4 and scores[i]>0.9:
-            motorcycle_boxes.append(box)
-
-    return np.array(motorcycle_boxes)
+    # return the intersection over union value
+    if (iou>0):
+        return iou
 
 ###########################################################################################################################
 # Root directory of the project
@@ -85,31 +77,40 @@ MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 IMAGES_DIR = os.path.join(ROOT_DIR, "images")
 
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+YOLOV3_WEIGHTS_PATH = os.path.join(ROOT_DIR, "yolov3.weights")
 
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
+# Local path to the YOLO configuration file
+YOLOV3_CFG_PATH = os.path.join(ROOT_DIR, "yolov3.cfg")
+
+# Download YOLO  weights if needed
+if not os.path.exists(YOLOV3_WEIGHTS_PATH):
+    print(YOLOV3_WEIGHTS_PATH, "has not been found")
+    print("Downloading YOLOv3 weights from https://pjreddie.com/media/files/yolov3.weights...")
+    urllib.request.urlretrieve("https://pjreddie.com/media/files/yolov3.weights", YOLOV3_WEIGHTS_PATH)
+    print("Weights have been downloaded and saved to", YOLOV3_WEIGHTS_PATH)
 
 # Create folder for images
 if not os.path.exists(IMAGES_DIR):
     os.mkdir(IMAGES_DIR)
 
-# Create a Mask-RCNN model in inference mode
-model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())    
+# Load YOLO
+net = cv2.dnn.readNet(YOLOV3_WEIGHTS_PATH, YOLOV3_CFG_PATH)
 
-# Load pre-trained model
-model.load_weights(COCO_MODEL_PATH, by_name=True)
+# Defin output layers
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
 
 # Load the video file we want to run detection on
 video_capture = VideoCapture(VIDEO_SOURCE)
 
+overlapped_frames = 0
+
 # Loop over each frame of video
 # counter = 0
 while True:
-    frame = video_capture.read()
+    original_frame = video_capture.read()
 
-    if frame is None:
+    if original_frame is None:
         break
     
     # Crop the image
@@ -117,65 +118,98 @@ while True:
     # y = frame.shape[1]-800
     # frame = frame[x:-1, y:-1]
     
-    # Convert the image from BGR color (which OpenCV uses) to RGB color
-    rgb_image = frame[:, :, ::-1]
+    # Resize the image
+    # scale = 0.3
+    # width = int(original_frame.shape[1] * scale)
+    # height = int(original_frame.shape[0] * scale)
+    frame = cv2.resize(original_frame, None, fx=0.4, fy=0.3)
+    height, width, channels = frame.shape
+    #frame = original_frame
+        
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0,0,0), True, False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
     
-    # Run the image through the Mask R-CNN model to get results.
-    results = model.detect([rgb_image], verbose=0)
+    class_ids=[]
+    confidences=[]
+    boxes=[]
 
-    # Mask R-CNN assumes we are running detection on multiple images.
-    # We only passed in one image to detect, so only grab the first result.
-    r = results[0]
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+                #cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
 
-    # The r variable will now have the results of detection:
-    # - r['rois'] are the bounding box of each detected object
-    # - r['class_ids'] are the class id (type) of each detected object
-    # - r['scores'] are the confidence scores for each detection
-    # - r['masks'] are the object masks for each detected object (which gives you the object outline)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
 
-    # Get boxes of all detected motorcycles
-    motorcycle_boxes = get_motorcycle_boxes(r['rois'], r['class_ids'], r['scores'])
-    print("Motorcycles detected: ", len(motorcycle_boxes))
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.6)
 
-    # Get boxes of all other objects
-    other_objects_boxes = get_other_objects_boxes(r['rois'], r['class_ids'])
-    print("Other objects detected: ", len(other_objects_boxes))
+    people_boxes = []
+    motorcycle_boxes = []
+    
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x,y,w,h = boxes[i]
+            if class_ids[i]==0: # Get boxes of all detected people
+                people_boxes.append([x, y, x+w, y+h])
+            elif class_ids[i]==3: # Get boxes of all detected motorcycles
+                motorcycle_boxes.append([x, y, x+w, y+h])
+    
+    print("Motorcycles detected:", len(motorcycle_boxes))
+    print("People detected:", len(people_boxes))
     
     # Draw green rectangles around motorcycles
     for box in motorcycle_boxes:
-        y1, x1, y2, x2 = box
+        x1, y1, x2, y2 = box
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-    # Draw red rectangles around other objects
-    for box in other_objects_boxes:
-        y1, x1, y2, x2 = box
+    # Draw red rectangles around people
+    for box in people_boxes:
+        x1, y1, x2, y2 = box
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 1)
-
-    # See how much those objects overlap with the motorcycles
-    overlaps = mrcnn.utils.compute_overlaps(motorcycle_boxes, other_objects_boxes)
 
     # Assume no overlaps with motorcyles
     is_overlap_detected = False
 
-    for overlap in overlaps:
-        # For this motorcycle, find the max amount it was covered by any
-        # other objects  that was detected in our image
-        max_IoU_overlap = np.max(overlap)
-        print("Max overlap: ", max_IoU_overlap)
-        if max_IoU_overlap > 0.2:
-            is_overlap_detected = True
-
+    # Detecting overlaps
+    for motorcycle_box in motorcycle_boxes:
+        for people_box in people_boxes:
+            if getIoU(motorcycle_box, people_box)>=0.2:
+                is_overlap_detected = True
+                break
+        if is_overlap_detected:
+            break
     print("Is overlap detected: ", is_overlap_detected)
 
     # If overlap is detected save the frame to the file and beep (Windows)
     if is_overlap_detected:
+        overlapped_frames += 1
+    else:
+        # reset overlap counter
+        overlapped_frames = 0
+
+    if overlapped_frames==3:
+        overlapped_frames = 0
         filename = datetime.datetime.now().strftime('%G-%m-%d_%H-%M-%S_overlap.jpg')
-        cv2.imwrite(os.path.join(IMAGES_DIR, filename), frame)
+        cv2.imwrite(os.path.join(IMAGES_DIR, filename), original_frame)
         winsound.Beep(2500, 1000)
 
-    # Uncomment the lines below to see the result in real time
-    # cv2.imshow('frame', frame)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break    
+    # Uncomment the code below to see the result in real time
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break    
 
 cv2.destroyAllWindows()
